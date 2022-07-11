bba9f0d791e17f17821981f3b90cca3533d98f98
David <scriptura7773@gmail.com>
2022-07-10 17:00:53 -0700

    validate password function boiler plate

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,10 +2,18 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
 
 const Register = () => {
 
-  const validatePasswrod = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [dateOfBirth, setDateOfBirth] = useState('')
 
+  const validatePasswrod = () => {
+    let isValid = true;
+    if (password !== '' && confirm)
   }    
-  
+
     return (
         <h1>Register</h1>
     )
