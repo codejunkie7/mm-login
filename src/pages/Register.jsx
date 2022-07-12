e4706a53d6d660707ed96253131f3aed5b4f0acb
David <scriptura7773@gmail.com>
2022-07-12 15:26:24 -0700

    styling update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,26 +1,42 @@
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../backend/firebase"
 import { useState, useEffect } from "react"
+import { Button } from "@mui/material"
 
 const Register = () => {
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [dateOfBirth, setDateOfBirth] = useState('')
-  // I love commits
-//   const validatePasswrod = () => {
-//     let isValid = true;
-//     if (password !== '' && confirm) {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [dateOfBirth, setDateOfBirth] = useState("")
+  const [error, setError] = useState("")
+  
+  // Function to ensure password input is correct
+  const validatePassword = () => {
+    let isValid = true;
+    if (password !== "" && confirmPassword !== "") {
+      if (password !== confirmPassword) {
+        isValied = false;
+        setError("Passwords do not match")
+      }   
+    }
+    return isValid
+  }    
 
-//     }
-//   }    
+  const register = (e) => {
+    e.preventDefault();
+    if (validatePassword()) {
+        // Creates a new user with email and password into the FireBase Auth
+        createUserWithEmailAndPassword()
+    }
+  }
 
     return (
         <div className="register">
             <div className="register-auth">
-                <form>
+                <form onSubmit={register}>
                     <input
                       type="email"
                       value={email}
@@ -68,6 +84,7 @@ const Register = () => {
                       required
                       onChange={e => setDateOfBirth(e.target.value)}
                     />
+                    <Button variant="outlined" type="submit">Register</Button>
                 </form>
 
             </div>
