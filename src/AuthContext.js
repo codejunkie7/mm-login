fe1a5a33652bef665482f8fa8eea044bc5cde5f5
David <scriptura7773@gmail.com>
2022-07-13 00:15:46 -0700

    finished context

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -2,3 +2,14 @@ import { useContext, createContext } from "react";
 
 const AuthContext = createContext()
 
+export function ProviderAuth ({children, value}) {
+  return (
+    <AuthContext.Provider value={value}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuthValue () {
+    return useContext(AuthContext)
+}
